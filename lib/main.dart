2a555c90a1244d2eda3f3fb7cc4f46eb36d2a62a
Yao Josué <jyao2000@yahoo.fr>
2020-05-20 02:48:02 +0200

    update question constructor and create quizz logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quizz_brain.dart';
 import 'question.dart';
 
 void main() => runApp(Quizzler());
@@ -37,11 +38,7 @@ class _QuizPageState extends State<QuizPage> {
     ),
   ];
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
+
 
   int questionIndex = 0;
 
--- lib/question.dart
+++ lib/question.dart
@@ -2,7 +2,12 @@ class Question{
   String questionText;
   bool questionAnswer;
 
-  Question({String q, bool a}){
+  /*Question({String q, bool a}){
+    questionText = q;
+    questionAnswer = a;
+  }*/
+
+  Question(String q, bool a){
     questionText = q;
     questionAnswer = a;
   }
