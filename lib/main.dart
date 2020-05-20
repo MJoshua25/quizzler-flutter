8f99aa8f64538b7d449e7c0d054b6dceabf40930
Yao Josué <jyao2000@yahoo.fr>
2020-05-20 15:55:43 +0200

    empty scoreKeeper

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _QuizPageState extends State<QuizPage> {
 
     Alert(context: context, title: "Félicitations", desc: "Vous avez terminé le quizz avec succès.").show();
     quizzBrain.reset();
+    scoreKeeper.clear();
     setState(() {
       if (quizzBrain.getQuestionAnswer() == userPickedAnswer) {
         scoreKeeper.add(
