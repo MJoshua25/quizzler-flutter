ddfe466532705b723661974702741dd20a8449f3
Yao Josué <jyao2000@yahoo.fr>
2020-05-20 02:32:59 +0200

    Fix errors

--- lib/main.dart
+++ lib/main.dart
@@ -37,18 +37,10 @@ class _QuizPageState extends State<QuizPage> {
     ),
   ];
 
-  Question q1 = Question(q: 'You can lead a cow down stairs but not up stairs.', a: false);
-
-  List<Question> questions = [
+  List<Question> questionBank = [
     Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
     Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
     Question(q: 'A slug\'s blood is green.', a: true),
-  ];
-
-  List<bool> answers = [
-    false,
-    true,
-    true,
   ];
 
   int questionIndex = 0;
@@ -65,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionIndex],
+                questionBank[questionIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -90,7 +82,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer = answers[questionIndex];
+                bool correctAnswer = questionBank[questionIndex].questionAnswer;
                 if (correctAnswer==false){
                   print('Bonne réponse');
                 } else{
